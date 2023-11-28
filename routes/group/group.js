c5cde391afe83291c6c716ccfe490e3d601bc63c
Ubuntu <ubuntu@ip-172-31-38-129.ap-northeast-2.compute.internal>
2023-11-28 16:35:44 +0900

    merge?

--- routes/group/group.js
+++ routes/group/group.js
@@ -281,7 +281,7 @@ router.post('/select', async (req, res) => {
   const userIdFromToken = req.user.user_id;
 
   try {
-    var sql = "SELECT group_id, is_password, FROM kbow.group_info WHERE group_name=?";
+    var sql = "SELECT group_id, is_password FROM kbow.group_info WHERE group_name=?";
     const insert_value = [req.body.title];
     let result = await mariaQuery(sql, insert_value);
     res.send(result);
